100d80aa95a5ac255223d2571b8fe1ff94379cd2
Cédric Phung <cedric3011@outlook.fr>
2023-11-08 20:51:22 +0100

    add fiels

--- src/Controller/Admin/ServiceCrudController.php
+++ src/Controller/Admin/ServiceCrudController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use Vich\UploaderBundle\Form\Type\VichFileType;
 use App\Entity\Service;
 use App\Form\Type\ServiceImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\{IdField, TextField, CollectionField, MoneyField};
+use EasyCorp\Bundle\EasyAdminBundle\Field\{IdField, TextField, CollectionField, MoneyField, ImageField, TextareaField, IntegerField};
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class ServiceCrudController extends AbstractCrudController
 {
@@ -18,8 +20,8 @@ class ServiceCrudController extends AbstractCrudController
     public function configureCrud(Crud $crud) : Crud 
     {
         return $crud
-            ->setEntityLabelInPlural('Serivces')
-            ->setEntityLabelInSingular('Serivce')
+            ->setEntityLabelInPlural('Services')
+            ->setEntityLabelInSingular('Service')
 
             ->setPageTitle("index", "Page d'administration des services")
             ->setPageTitle("edit", "Page de modification d'un service");
@@ -32,14 +34,18 @@ class ServiceCrudController extends AbstractCrudController
                 ->hideOnForm(),
             TextField::new('title')
                 ->setLabel('Titre'),
-            MoneyField::new('price')->setCurrency('EUR')
+            MoneyField::new('price')
+                ->setCurrency('EUR')
+                ->setNumDecimals(0)
                 ->setLabel('Prix'),
             TextField::new('content')
                 ->setLabel('Description'),
-            CollectionField::new('ServiceImage')
-                ->setLabel('Image')
-                ->setEntryType(ServiceImageType::class)
-                ->hideOnIndex(),
+            TextField::new('imageName')
+                ->setLabel('Nom de l\'image'),
+            TextField::new('imageFile')
+                ->setFormType(VichFileType::class),
+            IntegerField::new('size')
+                ->hideOnForm(),
         ];
         return $fields;
     }
