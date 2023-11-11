262246f392cd3061fc4fe112e48feb3fe0ec2de8
Cédric Phung <cedric3011@outlook.fr>
2023-11-11 14:14:32 +0100

    modify fields

--- src/Controller/Admin/AdCrudController.php
+++ src/Controller/Admin/AdCrudController.php
@@ -5,7 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Ad;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\{IdField, TextField, DateField, MoneyField, IntegerField};
+use EasyCorp\Bundle\EasyAdminBundle\Field\{IdField, TextField, DateField, MoneyField, IntegerField, FormField};
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class AdCrudController extends AbstractCrudController
@@ -28,6 +28,7 @@ class AdCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         $fields = [
+            FormField::addTab('Détails de l\'annonce'),
             IdField::new('id')
                 ->hideOnForm(),
             TextField::new('title')
@@ -35,20 +36,44 @@ class AdCrudController extends AbstractCrudController
             TextField::new('brand')
                 ->setLabel('Marque'),
             MoneyField::new('price')->setCurrency('EUR')
-                ->setLabel('Prix'),
+                ->setCurrency('EUR')
+                ->setNumDecimals(0)
+                ->setLabel('Prix')
+                ->setCustomOption('storedAsCents', false),
             IntegerField::new('kilometers')
                 ->setLabel('Kilométrage'),
             DateField::new('registrationYear')->renderAsNativeWidget()
                 ->setLabel('Année de mise en circulation'),
-            TextField::new('slug'),
             DateField::new('createdAt')
                 ->hideOnForm(),
+
+            FormField::addTab('Images'),
+            FormField::addFieldset(),
+
+            TextField::new('imageRename1')
+            ->setLabel('Nom de l\'image 1')
+            ->hideOnIndex(),
             TextField::new('imageFile1')
-                ->setFormType(VichFileType::class),
+                ->setFormType(VichFileType::class)
+                ->setLabel('Image 1')
+                ->hideOnIndex(),
+            FormField::addFieldset(),
+            TextField::new('imageRename2')
+                ->setLabel('Nom de l\'image 2')
+                ->hideOnIndex(),
             TextField::new('imageFile2')
-                ->setFormType(VichFileType::class),
-            IntegerField::new('size')
-                ->hideOnForm(),
+                ->setFormType(VichFileType::class)
+                ->setLabel('Image 2')
+                ->hideOnIndex(),
+
+            FormField::addFieldset(),
+            TextField::new('imageRename3')
+                ->setLabel('Nom de l\'image 3')
+                ->hideOnIndex(),
+            TextField::new('imageFile3')
+                ->setFormType(VichFileType::class)
+                ->setLabel('Image 3')
+                ->hideOnIndex(),
         ];
         return $fields;
     }
