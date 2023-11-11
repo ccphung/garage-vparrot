0569edb4f8ac0c53aea73734100ea55a06b28ce8
Cédric Phung <cedric3011@outlook.fr>
2023-11-11 10:15:05 +0100

    create service data fixtures

--- src/DataFixtures/ServiceFixtures.php
+++ src/DataFixtures/ServiceFixtures.php
@@ -14,13 +14,32 @@ class ServiceFixtures extends Fixture
     {
             $mecanique = new Service();
             $mecanique->setTitle('Réparation mécanique');
-            $mecanique->setContent('Réparation');
-            $mecanique->setImageName('Réparation');
-            $mecanique->setPrice('80');
-            $mecanique->setImageFile(new UploadedFile('public/images/services/entretien.png','entretien.png','png', UPLOAD_ERR_OK, true));
+            $mecanique->setContent('Expert en réparation toutes marques, voitures électriques comprises, notre garage automobile est en mesure d’effectuer les différentes réparations');
+            $mecanique->setImageName('reparation-meca');
+            $mecanique->setPrice('92');
+            $mecanique->setImageFile(new UploadedFile('public/images/services/reparation-mecanique.png','reparation-mecanique.png','png', UPLOAD_ERR_OK, true));
             $mecanique->setUpdatedAt(new DateTimeImmutable());
-
             $manager->persist($mecanique);
+
+
+            $entretien = new Service();
+            $entretien->setTitle('Entretien');
+            $entretien->setContent('La révision et la vidange sont des étapes clés dans l’entretien de votre véhicule. Elles préservent votre moteur et en assure sa longévité. Nous vous recommandons d’effectuer une vidange tous les ans ou tous les 15000km.');
+            $entretien->setImageName('Entretien');
+            $entretien->setPrice('65');
+            $entretien->setImageFile(new UploadedFile('public/images/services/entretien.png','entretien.png','png', UPLOAD_ERR_OK, true));
+            $entretien->setUpdatedAt(new DateTimeImmutable());
+            $manager->persist($entretien);
+
+            $carrosserie = new Service();
+            $carrosserie->setTitle('Réparation carrosserie');
+            $carrosserie->setContent('Jantes éraflées, griffure sur le tableau de bord ou intérieur de portière, bosse ou rayure sur votre carrosserie voiture…? Confiez nous la réparation carrosserie de votre véhicule.');
+            $carrosserie->setImageName('reparation-carrosserie');
+            $carrosserie->setPrice('120');
+            $carrosserie->setImageFile(new UploadedFile('public/images/services/reparation-carrosserie.png','reparation-carrosserie.png','png', UPLOAD_ERR_OK, true));
+            $carrosserie->setUpdatedAt(new DateTimeImmutable());
+
+            $manager->persist($carrosserie);
             $manager->flush();
         }
 }
