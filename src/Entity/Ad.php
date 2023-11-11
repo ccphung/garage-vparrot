a572325292e463b6b1ffa5d85980faea8916e108
Cédric Phung <cedric3011@outlook.fr>
2023-11-11 14:15:16 +0100

    add new fields and remove slug

--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -19,13 +19,15 @@ class Ad
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
-    
-    #[Vich\UploadableField (mapping: 'ads', fileNameProperty: 'imageName2', size: 'size2')]
-    private ?File $imageFile2 = null;
         
-    #[Vich\UploadableField (mapping: 'ads', fileNameProperty: 'imageName1', size: 'size1')]
+    #[Vich\UploadableField (mapping: 'ads2', fileNameProperty: 'imageName2', size: 'size1')]
     private ?File $imageFile1 = null;
+
+    #[Vich\UploadableField (mapping: 'ads1', fileNameProperty: 'imageName1', size: 'size2')]
+    private ?File $imageFile2 = null;
+
+    #[Vich\UploadableField (mapping: 'ads3', fileNameProperty: 'imageName3', size: 'size3')]
+    private ?File $imageFile3 = null;
 
 
     #[ORM\Column(length: 255)]
@@ -55,20 +57,35 @@ class Ad
     #[ORM\Column]
     private ?\DateTimeImmutable $updatedAt2 = null;
 
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt3 = null;
+
     #[ORM\Column(length: 255)]
     private ?string $imageName1 = null;
-
-    #[ORM\Column]
-    private ?int $size1 = null;
 
     #[ORM\Column(length: 255)]
     private ?string $imageName2 = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $imageName3 = null;
+
+    #[ORM\Column]
+    private ?int $size1 = null;
+
     #[ORM\Column]
     private ?int $size2 = null;
 
+    #[ORM\Column]
+    private ?int $size3 = null;
+
     #[ORM\Column(length: 255)]
-    private ?string $slug = null;
+    private ?string $imageRename1 = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imageRename2 = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imageRename3 = null;
 
     public function getId(): ?int
     {
@@ -81,18 +98,29 @@ class Ad
         $this->updatedAt1 = new \DateTimeImmutable();
     }
 
-    public function getImageFile1(): ?File {
-        return $this->imageFile1;
-    }
-
+    
     public function setImageFile2(?File $imageFile2) : void
     {
         $this->imageFile2 = $imageFile2;
         $this->updatedAt2 = new \DateTimeImmutable();
     }
 
+    public function setImageFile3(?File $imageFile3) : void
+    {
+        $this->imageFile3 = $imageFile3;
+        $this->updatedAt3 = new \DateTimeImmutable();
+    }
+
+    public function getImageFile1(): ?File {
+        return $this->imageFile1;
+    }
+
     public function getImageFile2(): ?File {
         return $this->imageFile2;
+    }
+
+    public function getImageFile3(): ?File {
+        return $this->imageFile3;
     }
 
     public function getTitle(): ?string
@@ -188,40 +216,21 @@ class Ad
         return $this->imageName1;
     }
 
+    public function getImageName2(): ?string
+    {
+        return $this->imageName2;
+    }
+    
+    public function getImageName3(): ?string
+    {
+        return $this->imageName3;
+    }
+
     public function setImageName1(?string $imageName1): static
     {
         $this->imageName1 = $imageName1;
 
         return $this;
-    }
-
-    public function getSize1(): ?int
-    {
-        return $this->size1;
-    }
-
-    public function setSize1(?int $size1): static
-    {
-        $this->size1 = $size1;
-
-        return $this;
-    }
-
-    public function getUpdatedAt1(): ?\DateTimeImmutable
-    {
-        return $this->updatedAt1;
-    }
-
-    public function setUpdatedAt1(\DateTimeImmutable $updatedAt1): static
-    {
-        $this->updatedAt1 = $updatedAt1;
-
-        return $this;
-    }
-
-    public function getImageName2(): ?string
-    {
-        return $this->imageName2;
     }
 
     public function setImageName2(?string $imageName2): static
@@ -231,9 +240,33 @@ class Ad
         return $this;
     }
 
+    public function setImageName3(?string $imageName3): static
+    {
+        $this->imageName3 = $imageName3;
+
+        return $this;
+    }
+    
+    public function getSize1(): ?int
+    {
+        return $this->size1;
+    }
+
     public function getSize2(): ?int
     {
         return $this->size2;
+    }
+
+    public function getSize3(): ?int
+    {
+        return $this->size3;
+    }
+
+    public function setSize1(?int $size1): static
+    {
+        $this->size1 = $size1;
+
+        return $this;
     }
 
     public function setSize2(?int $size2): static
@@ -243,9 +276,33 @@ class Ad
         return $this;
     }
 
+    public function setSize3(?int $size3): static
+    {
+        $this->size3 = $size3;
+
+        return $this;
+    }
+
+    public function getUpdatedAt1(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt1;
+    }
+
     public function getUpdatedAt2(): ?\DateTimeImmutable
     {
         return $this->updatedAt2;
+    }
+
+    public function getUpdatedAt3(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt3;
+    }
+    
+    public function setUpdatedAt1(\DateTimeImmutable $updatedAt1): static
+    {
+        $this->updatedAt1 = $updatedAt1;
+
+        return $this;
     }
 
     public function setUpdatedAt2(\DateTimeImmutable $updatedAt2): static
@@ -255,20 +312,48 @@ class Ad
         return $this;
     }
 
-    public function getSlug(): ?string
+    public function setUpdatedAt3 (\DateTimeImmutable $updatedAt3): static
     {
-        return $this->slug;
-    }
-
-    public function setSlug(string $slug): static
-    {
-        $this->slug = $slug;
+        $this->updatedAt3 = $updatedAt3;
 
         return $this;
     }
 
+    public function getImageRename1(): ?string
+    {
+        return $this->imageRename1;
+    }
+
+    public function setImageRename1(string $imageRename1): static
+    {
+        $this->imageRename1 = $imageRename1;
+
+        return $this;
+    }
+
+    public function getImageRename2(): ?string
+    {
+        return $this->imageRename2;
+    }
+
+    public function setImageRename2(string $imageRename2): static
+    {
+        $this->imageRename2 = $imageRename2;
+
+        return $this;
+    }
+
+    public function getImageRename3(): ?string
+    {
+        return $this->imageRename3;
+    }
+
+    public function setImageRename3(string $imageRename3): static
+    {
+        $this->imageRename3 = $imageRename3;
+
+        return $this;
+    }
 }
 
-class Adpic extends Ad {
-    
-}
+
