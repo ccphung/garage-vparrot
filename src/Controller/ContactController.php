a166ded66f0e34421d7efbe909d936ba82e6d161
Cédric Phung <cedric3011@outlook.fr>
2023-11-13 12:26:14 +0100

    create contact page

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\OpeningHoursRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(OpeningHoursRepository $openingHours, Request $request, EntityManagerInterface $manager): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $manager->persist($contact);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre message a bien été envoyé'
+            );
+
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'horaires' => $openingHours->findOneBy([], ['id' => 'asc']),
+            'form' => $form->createView()
+        ]);
+    }
+}
