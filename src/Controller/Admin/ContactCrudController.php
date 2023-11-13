6d4b0d124a501679f1f06796a2174083445af50a
Cédric Phung <cedric3011@outlook.fr>
2023-11-13 19:38:39 +0100

    configure fields

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -5,8 +5,11 @@ namespace App\Controller\Admin;
 use App\Entity\Contact;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use Symfony\Component\Security\Http\Attribute\IsGranted as AttributeIsGranted;
 
 #[AttributeIsGranted('ROLE_USER')]
@@ -17,14 +20,29 @@ class ContactCrudController extends AbstractCrudController
         return Contact::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->hideOnForm()
+                ->hideOnIndex(),
+            TextField::new('firstName')
+                ->setLabel('Prénom'),
+            TextField::new('lastName')
+                ->setLabel('Nom'),
+            EmailField::new('email')
+                ->setLabel('Email'),
+            TelephoneField::new('phoneNumber')
+                ->setLabel('Téléphone'),
+            TextField::new('subject')
+                ->setLabel('Sujet'),
+            TextField::new('message')
+                ->setLabel('message'),
+            DateField::new('createdAt')
+                ->setLabel('Date'),
+            BooleanField::new('isProcessed')
+                ->setLabel('Traité'),
         ];
     }
-    */
+
 }
