072c8a0e046bf6643aa294483b31fee03e24f2cb
Cédric Phung <cedric3011@outlook.fr>
2023-11-13 18:03:24 +0100

    add contact form

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Entity\Contact;
+use App\Form\ContactType;
 use App\Repository\AdRepository;
 use App\Repository\OpeningHoursRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,11 +25,31 @@ class AdController extends AbstractController
     }
 
     #[Route('/annonce/{title}', name: 'details')]
-    public function details(Ad $ad, OpeningHoursRepository $openingHours): Response
+    public function details(Ad $ad, OpeningHoursRepository $openingHours, Contact $contact, Request $request, EntityManagerInterface $manager): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($contact);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre message a bien été envoyé'
+            );
+            unset($contact);
+            unset($form);
+            $contact = new Contact();
+            $form = $this->createForm(ContactType::class, $contact);
+        }
+        
+
         return $this->render('ad/detail.html.twig', [
             'ad' => $ad,
             'horaires' => $openingHours->findOneBy([], ['id' => 'asc']),
+            'form' => $form->createView()
         ]);
                 
     }
