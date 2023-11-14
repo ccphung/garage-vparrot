5e7146338e53ed07bc5ff5103be20882b70e5e7b
Cédric Phung <cedric3011@outlook.fr>
2023-11-14 12:55:29 +0100

    modify review crud controller

--- src/Controller/Admin/ReviewCrudController.php
+++ src/Controller/Admin/ReviewCrudController.php
@@ -7,24 +7,47 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use Symfony\Component\Security\Http\Attribute\IsGranted as AttributeIsGranted;
 
 #[AttributeIsGranted('ROLE_USER')]
 class ReviewCrudController extends AbstractCrudController
 {
+    public function configureCrud(Crud $crud) : Crud 
+    {
+        return $crud
+            ->setEntityLabelInPlural('Avis')
+            ->setEntityLabelInSingular('Avis')
+
+            ->setPageTitle("index", "Gestion des avis")
+            ->setPageTitle("edit", "Gestion d'un avis")
+
+            ->setPaginatorPageSize(10);
+    }
+
     public static function getEntityFqcn(): string
     {
         return Review::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->hideOnForm()
+                ->hideOnIndex(),
+            TextField::new('firstName')
+                ->setLabel('Prénom'),
+            TextField::new('lastName')
+                ->setLabel('Nom'),
+            TextEditorField::new('comment')
+                ->setLabel('Commentaire'),
+            DateField::new('createdAt')
+                ->setLabel('Date'),
+            BooleanField::new('isApproved')
+                ->setLabel('Approuvé'),
         ];
     }
-    */
 }
