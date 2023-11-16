4d1ccdd437611a2702a12b22f2c4984e7a860382
Cédric Phung <cedric3011@outlook.fr>
2023-11-16 12:24:24 +0100

    configure fields

--- src/Controller/Admin/AnnouncementCrudController.php
+++ src/Controller/Admin/AnnouncementCrudController.php
@@ -5,8 +5,9 @@ namespace App\Controller\Admin;
 use App\Entity\Announcement;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use Symfony\Component\Security\Http\Attribute\IsGranted as AttributeIsGranted;
 
 #[AttributeIsGranted('ROLE_USER')]
@@ -17,14 +18,28 @@ class AnnouncementCrudController extends AbstractCrudController
         return Announcement::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud) : Crud 
+    {
+        return $crud
+            ->setEntityLabelInPlural('Annonces spéciales')
+            ->setEntityLabelInSingular('Annonce spéciale')
+
+            ->setPageTitle("index", "Gestion des annonces spéciales")
+            ->setPageTitle("edit", "Gestion d'une annonce spéciale")
+
+            ->setPaginatorPageSize(10);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->hideOnForm()
+                ->hideOnIndex(),
+            TextareaField::new('Text')
+                ->setLabel('Message'),
+            BooleanField::new('isPublished')
+                ->setLabel('Publier'),
         ];
     }
-    */
 }
