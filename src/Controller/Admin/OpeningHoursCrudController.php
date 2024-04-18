fd5168779ed34bfc95aab05f8837ecb19e2c321d
Cédric Phung <cedric3011@outlook.fr>
2024-04-18 14:30:22 +0200

    modify display

--- src/Controller/Admin/OpeningHoursCrudController.php
+++ src/Controller/Admin/OpeningHoursCrudController.php
@@ -32,55 +32,104 @@ class OpeningHoursCrudController extends AbstractCrudController
             ->hideOnForm()
             ->hideOnIndex(),
             TextField::new('mon_am_open')
-                ->setLabel('Lun mat ouverture'),
+                ->setLabel('Lun mat ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('mon_am_close')
-                ->setLabel('Lun mat fermeture'),
+                ->setLabel('Lun mat fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('mon_pm_open')
-                ->setLabel('Lun ap ouverture'),
+                ->setLabel('Lun ap ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('mon_pm_close')
-                ->setLabel('Lun ap fermeture'),
+                ->setLabel('Lun ap fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('tue_am_open')
-                ->setLabel('Mar mat ouverture'),
+                ->setLabel('Mar mat ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('tue_am_close')
-                ->setLabel('Mar mat fermeture'),
+                ->setLabel('Mar mat fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('tue_pm_open')
-                ->setLabel('Mar ap ouverture'),
+                ->setLabel('Mar ap ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('tue_pm_close')
-                ->setLabel('Mar ap fermeture'),
+                ->setLabel('Mar ap fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('wed_am_open')
-                ->setLabel('Mer mat ouverture'),
+                ->setLabel('Mer mat ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('wed_am_close')
-                ->setLabel('Mer mat fermeture'),
+                ->setLabel('Mer mat fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('wed_pm_open')
-                ->setLabel('Mer ap ouverture'),
+                ->setLabel('Mer ap ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('wed_pm_close')
-                ->setLabel('Mer ap fermeture'),
+                ->setLabel('Mer ap fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('thr_am_open')
-                ->setLabel('Jeu mat ouverture'),
+                ->setLabel('Jeu mat ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('thr_am_close')
-                ->setLabel('Jeu mat fermeture'),
+                ->setLabel('Jeu mat fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('thr_pm_open')
-                ->setLabel('Jeu ap ouverture'),
+                ->setLabel('Jeu ap ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('thr_pm_close')
-                ->setLabel('Jeu ap fermeture'),
+                ->setLabel('Jeu ap fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('fri_am_open')
-                ->setLabel('Ven mat ouverture'),
+                ->setLabel('Ven mat ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('fri_am_close')
-                ->setLabel('Ven mat fermeture'),
+                ->setLabel('Ven mat fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('fri_pm_open')
-                ->setLabel('Ven ap ouverture'),
+                ->setLabel('Ven ap ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('fri_pm_close')
-                ->setLabel('Ven ap fermeture'),
+                ->setLabel('Ven ap fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('sat_am_open')
-                ->setLabel('Sam mat ouverture'),
+                ->setLabel('Sam mat ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('sat_am_close')
-                ->setLabel('Sam mat fermeture'),
+                ->setLabel('Sam mat fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('sat_pm_open')
-                ->setLabel('Sam ap ouverture'),
+                ->setLabel('Sam ap ouverture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('sat_pm_close')
-                ->setLabel('Sam ap fermeture'),
+                ->setLabel('Sam ap fermeture')
+                ->setColumns('col-sm-3')
+                ->setCssClass('col-md-3'),
             TextField::new('sun')
-                ->setLabel('Dimanche'),
+                ->setLabel('Dimanche')
+                ->setColumns('col-sm-3'),
         ];
     }
 
